11cfd6e6cf83141055a5d53f3f979e694883b1c4
narraccino <giuseppenarracci@gmail.com>
2018-06-10 21:16:55 +0200

    Build 1.3

--- MP.py
+++ MP.py
@@ -4,10 +4,7 @@ from IPython.display import display
 from IPython.display import Image
 
 
-#print(raw_data)
-#print(type(raw_data), type(df_a))
-# arr= min(raw_data['A'])
-# raw_data['A']= arr
+
 
 def mostPleasure(raw_data):
 
@@ -16,17 +13,3 @@ def mostPleasure(raw_data):
         raw_data[key]=maximo
 
     return raw_data
-
-
-# df_a = pd.DataFrame(raw_data,columns = ['A', 'B','C','D','E','F','G','H','I','J'],index=[0])
-#
-# print(df_a)
-# listOrdered= OrderedDict(sorted(raw_data.items(), reverse=True,key=lambda t: t[1]))
-#
-# #print(listOrdered)
-#
-# lista=[]
-# for key,value in listOrdered.items():
-#     lista.append(key)
-#
-# print(lista)
